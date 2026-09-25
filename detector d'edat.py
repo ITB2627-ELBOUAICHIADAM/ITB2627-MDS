bea06518481aecd62b40c9ce14b1e14d631e172d
ITB2627-ELBOUAICHIADAM <adam.elbouaichi.7ea@itb.cat>
2026-09-25 10:38:11 +0200

    detector d'edat

---
+++ detector d'edat.py
@@ -0,0 +1,10 @@
+"""
+Adam El Bouaichi El Attari
+25/09/2026
+ASIX1D tasca 02 MDS
+"""
+edad=int(input("Introdueix la teva edat: "))
+if edad>=18:
+    print("Ets major d'edat")
+else:
+    print("Ets menor d'edat")   
